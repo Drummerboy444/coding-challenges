a3a1726973b31ec3f40b21db81d6678481db09d1
Anthony Murphy <am13571@my.bristol.ac.uk>
2019-11-15 18:00:11 +0000

    1-Fixes so that both functions mutate the list

--- 1-havel-hakimi-algorithm/anthony/eliminate_zeros.py
+++ 1-havel-hakimi-algorithm/anthony/eliminate_zeros.py
@@ -1,3 +1,2 @@
 def eliminate_zeros(numbers):
-    return list(filter(lambda n: n != 0, numbers))
-    # return [i for i in numbers if i != 0]
+    numbers[:] = [i for i in numbers if i != 0]
--- 1-havel-hakimi-algorithm/anthony/main.py
+++ 1-havel-hakimi-algorithm/anthony/main.py
@@ -3,8 +3,6 @@ from sort_descending import sort_descending
 
 
 answers = [5, 3, 0, 2, 6, 2, 0, 7, 2, 5]
-
-
-zeros_eliminated = eliminate_zeros(answers)
-sort_descending(zeros_eliminated)
-print(zeros_eliminated)
+eliminate_zeros(answers)
+sort_descending(answers)
+print(answers)
